cd261a575df624a277a91ea39ca08eef4dd42d45
PedroY09 <92602616+PedroY09@users.noreply.github.com>
2021-10-22 17:00:31 -0300

    sextou com a idade

---
+++ sextaras.js
@@ -0,0 +1,21 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano){
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        }else if (fsex[1]){
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.  `
+    }
+
+
+}
