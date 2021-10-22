3c3d6583486973a7255a366ba404c9898decc574
PedroY09 <92602616+PedroY09@users.noreply.github.com>
2021-10-22 14:47:28 -0300

    chora boy

--- script.js
+++ script.js
@@ -1,79 +1,75 @@
-alert('Que horas são?!')
-//Puxamos a data e as horas
-var lm = new Date()
-var h = lm.getHours()
-var m = lm.getMinutes()
-var s = lm.getSeconds()
-var d = lm.getDay()
-var dia = lm.getDate()
-var mes = lm.getMonth()
-var ano = lm.getFullYear()
+function carregar(){
+    //chamar as info
+    var ln = new Date()
+    var data = ln.getDate()
+    var dia = ln.getDay()
+    var mes = ln.getMonth()
+    var ano = ln.getFullYear()
+    var hora = ln.getHours()
+    var minuto = ln.getMinutes()
+    var segundo = ln.getSeconds()
 
-//Puxamos o id das tags e transformamos em variavel 
-var txthr = document.getElementById('txthr')
-var back = document.getElementById('back')
-var txtdia = document.getElementById('txtdia')
-var hr = document.getElementById('hr')
-var sec = document.getElementById('sec')
-var daa = document.getElementById('daa')
-var fto = document.getElementById('fto')
+    //chamar as variaveis
+    var back = document.getElementById('back')
+    var hr = document.getElementById('hr')
+    var sec = document.getElementById('sec')
+    var txthr = document.getElementById('txthr')
+    var img = document.getElementById('img')
+    var txtdia = document.getElementById('txtdia')
+    var daa = document.getElementById('daa')
 
-//definido o dia e as horas
-daa.innerHTML = `${dia}/${mes}/${ano}`
-txthr.innerHTML = `Agora são exatamente ${h}:${m}:${s}`
 
-//Condição para mudar o nome do momento do dia e o background da section
-if(h >= 0 && h < 5){
-    hr.innerHTML = 'Madrugada'
-    sec.style.background = '#B0E0E6'
-}else if (h >= 5 && h < 12){
-    hr.innerHTML = 'Manhã'
-    sec.style.background = '#FAFAD2'
-}else if(h >= 12 && h < 18){
-    hr.innerHTML = 'Tarde'
-    sec.style.background = '#FFE4B5'
-}else if(h >= 18 && h <0){
-    hr.innerHTML = 'Noite'
-    sec.style.background = '#E0FFFF'
-}
+    //difinir o dia de acordo com o dia atual
+    switch (dia){
+        case 0:
+            daa.innerHTML = 'Domingo'
+            break
+        case 1:
+            daa.innerHTML = 'Segunda'
+            break
+        case 2:
+            daa.innerHTML = 'Terça'
+            break
+        case 3:
+            daa.innerHTML = 'Quarta'
+            break
+        case 4:
+            daa.innerHTML = 'Quinta'
+            break
+        case 5:
+            daa.innerHTML = 'Sexta'
+            break
+        case 6:
+            daa.innerHTML = 'Sábado'
+            break
+    }
 
-//condição para mudar o background do body de acordo com a hr
-if (h >= 0 && h < 5){
-    back.style.background = '#00008B'
-    fto.src = 'madrugada.png'
-}else if(h >= 5 && h < 12){
-    back.style.background = '#FFA500'
-    fto.src = 'manha.png'
-}else if(h >= 12 && h < 18){
-    back.style.background = '#FF8C00'
-    fto.src = 'tarde.png'
-}else if(h >= 18 && h < 0){
-    back.style.background = '#191970'
-    fto.scr = 'noite.png'
-}
+    //mostrar a data e a hr
 
-//definir o dia de acordo com o dia atual
-switch (d){
-    case 0:
-        txtdia.innerHTML = 'Domingo'
-        break
-    case 1:
-        txtdia.innerHTML = 'Segunda'
-        break
-    case 2:
-        txtdia.innerHTML = 'Terça'
-        break
-    case 3:
-        txtdia.innerHTML = 'Quarta'
-        break
-    case 4:
-        txtdia.innerHTML = 'Quinta'
-        break
-    case 5:
-        txtdia.innerHTML = 'Sexta'
-        break
-    case 6:
-        txtdia.innerHTML = 'Sábado'
-        break
+    txthr.innerHTML = `${hora}:${minuto}:${segundo}`
+    txtdia.innerHTML = `${data}/${mes}/${ano}`
 
+    //mudar as configurações de acordo com a hr
+    if(hora >= 0 && hora < 5){
+        hr.innerHTML = 'Madrugada'
+        back.style.background = '#00008B'
+        sec.style.background = '#B0E0E6'
+        img.src = 'madrugada.png'
+    }else if(hora >= 5 && hora < 12){
+        hr.innerHTML = 'Manhã'
+        back.style.background = '#FFA500'
+        sec.style.background = '#FAFAD2'
+        img.src = 'manha.png'
+    }else if(hora >= 12 && hora <18){
+        hr.innerHTML = 'Tarde'
+        back.style.background = '#FF8C00'
+        sec.style.background = '#FFE4B5'
+        img.src = 'tarde.png'
+    }else{
+        hr.innerHTML = 'Noite'
+        back.style.background = '#191970'
+        sec.style.background = '#E0FFFF'
+        img.src = 'noite.png'
+    }
+    
 }
