4246ac266bde5e853b990d4e4549596d3ede694f
PedroY09 <92602616+PedroY09@users.noreply.github.com>
2021-11-05 10:12:41 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,15 @@
+function cadastro(){
+    //puxa as variaveis
+    var nome = document.getElementById('txt1')
+    var snome = document.getElementById('txt2')
+    var email = document.getElementById('email')
+    var senha = document.getElementById('senha1')
+    var senhaa = document.getElementById('senha2')
+    var botao = document.getElementById('botao')
+
+    if(nome.length < 8 || nome.length > 8)
+        alert('tamanho do nome invalido')
+     
+
+
+}
