0fbf8aa59c488ed9534e506a25d2969bf18aa897
PedroY09 <92602616+PedroY09@users.noreply.github.com>
2021-11-08 13:13:34 -0300

    Add files via upload

--- cadastro.js
+++ cadastro.js
@@ -1,15 +1,39 @@
-function cadastro(){
-    //puxa as variaveis
-    var nome = document.getElementById('txt1')
-    var snome = document.getElementById('txt2')
-    var email = document.getElementById('email')
-    var senha = document.getElementById('senha1')
-    var senhaa = document.getElementById('senha2')
-    var botao = document.getElementById('botao')
+function ok() {
+    var nome = (document.getElementById('nome').value)
+    var snome = (document.getElementById('snome').value)
+    var email = (document.getElementById('email').value)
+    var senha = (document.getElementById('senha').value)
+    var senhaa = (document.getElementById('senhaa').value)
 
-    if(nome.length < 8 || nome.length > 8)
-        alert('tamanho do nome invalido')
-     
+    
+    n_s(nome,snome)
+    e(email)
+    s_ss(senha,senhaa)
+}
+function n_s (n,s){
+
+    if(n.length <= 3 && s.length <= 3){
+        alert('errado')
+    }
+    if(n.search(/[0-9]/) <0 && s.search(/[0-9]/) <0){
+    
+    }else{
+        alert('somente letras')
+    }
+}
+function e(e){
+    if(e==''|| e.indexOf('@') == -1 || e.indexOf('.') == -1){
+        alert('email invalido')
+    }     
+
+}
+function s_ss (s , ss){
+    if(s.length >8){
+        alert('Não pode passar de 8 caracteres')
+    }
+    if(s.length <5){
+        alert('A senha tem que ter no minimo 8 caracteres')
+    }
 
 
 }
